278ed9d4cf9b9f5dbd178d13069117ac93549867
miriam <miriamdamianretamozo@gmail.com>
2022-07-14 10:31:14 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/user'){
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('users');
+    }else{
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hola Mundo');
+    }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
