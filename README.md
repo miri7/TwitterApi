a8bc3e67ec134280c3dc711fe9884a3aa340b4c4
miriam <miriamdamianretamozo@gmail.com>
2022-07-15 10:58:34 -0500

    add Route not found and error middelware

---
+++ README.md
@@ -0,0 +1,21 @@
+# Twitter API
+
+custom twitter API clone
+
+## Installation
+
+`npm install`
+
+
+# Configuration
+
+Copy `.env.example` to `.env` and set your enviroment variables
+
+## run
+`npm start`
+
+# development
+`npm run dev`
+
+# test
+`npm test`
--- server/index.js
+++ server/index.js
@@ -2,10 +2,28 @@ const express = require('express')
 const app = express()
 
 
+
 app.get('/', (req, res) => {
   res.json({
     message:'hola mundo'
   })
 })
+
+app.use((req,res,next)=>{
+  next({
+    statusCode:404,
+    message:'route not found'
+  })
+})
+
+app.use((err,req,res,next)=>{
+  const {statusCode = 500 , message = ''} = err
+  res.status(statusCode);
+  res.json({
+    message, 
+  })
+  
+})
+
 
 module.exports = app;
